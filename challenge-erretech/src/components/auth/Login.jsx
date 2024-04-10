edfdc41ce3550ff49293f434e271f78d5f797cf1
André Ferreira <110822583+andre9277@users.noreply.github.com>
2024-04-10 18:18:51 +0100

    Login function implemented
    After downloading the collection to postman and using axios to access the endpoint provided.
    The user could login with the message OK.

---
+++ challenge-erretech/src/components/auth/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import axios from "axios"; //axios library to handle api calls
+
+const Login = () => {
+  //attributes to save and handle information about the user
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.post(
+        "https://app.grupoerre.pt:1934/auth/login",
+        { email, password }
+      );
+      //if the login is successful
+      console.log(resp);
+      //console.log(resp.data);
+    } catch (error) {
+      //if the login finds some error
+      // console.error(error.resp.data);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      {/* Email field */}
+      <label>Email:</label>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      {/*   Password field */}
+      <label>Password:</label>
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {/* button to login  */}
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
