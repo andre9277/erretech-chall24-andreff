3a9e59b26ba1e6d2327276ec4a3f3eeb220c2cae
André Ferreira <110822583+andre9277@users.noreply.github.com>
2024-04-11 12:07:43 +0100

    Update Main Page
    Implemented button on main page for the user to chose between the Login or Register form.

--- challenge-erretech/src/App.jsx
+++ challenge-erretech/src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import "./App.css";
-//import Login from "./components/auth/Login";
+import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
 function App() {
+  //state that keeps track if the user is logged in or not
+  const [isLog, setIsLog] = useState(true);
+
+  const toggleLog = () => {
+    //change between login and register form
+    setIsLog(!isLog);
+  };
+
   return (
     <>
-      {/* <Login />{" "} */}
-      <Register />
+      <div>
+        <h1>{isLog ? "Login" : "Register"}</h1>
+        {isLog ? <Login /> : <Register />}
+        <button onClick={toggleLog}>
+          {" "}
+          {isLog ? "Go to Register" : "Go to Login"}{" "}
+        </button>
+      </div>
     </>
   );
 }
